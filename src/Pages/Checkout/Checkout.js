1ab361b8e99e44da39b4e73dcd7c58b5a100a5ea
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-04-18 12:26:31 +0600

    checkout updated

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Checkout = () => {
+    const [user] = useAuthState(auth);
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
+    // const navigate = useNavigate();
+
+    const handleNameBlur = event => {
+        setName(event.target.value);
+    }
+
+
+
+    const handleAddressBlur = event => {
+        setAddress(event.target.value);
+    }
+
+    const handlePhoneBlur = event => {
+        setPhone(event.target.value);
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+        const shipping = { name, email, address, phone };
+        console.log(shipping);
+    }
+
+    const checkoutBtn = () => {
+        if (Checkout) {
+            toast('Thank you for your booking ')
+        }
+    }
     return (
-        <div>
-            <h2>Please Checkout your booking</h2>
+        <div className='form-container register-form'>
+            <div style={{ margin: "20px" }}>
+                <h2 className='form-title text-danger'>Shipping Info</h2>
+                <form onSubmit={handleCreateUser}>
+                    <div className="input-group">
+
+                        <input className='mt-3   ' onBlur={handleNameBlur} type="text" name="name" id="" placeholder='Name' />
+                    </div>
+                    <div className="input-group">
+
+                        <input className='mt-3' value={user?.email} readOnly type="email" name="email" id="" required />
+                    </div>
+                    <div className='input-group'>
+
+                        <input className='mt-3' onBlur={handleAddressBlur} type="text" name="Address" id="" placeholder='Address' required />
+                    </div>
+                    <div className='input-group'>
+
+                        <input className='mt-3' onBlur={handlePhoneBlur} type="text" name="Phone" id="" placeholder='Phone' required />
+                    </div>
+                    <p style={{ color: 'red' }}>{error}</p>
+                    <input onClick={checkoutBtn} className='form-submit btn btn-danger' type="submit" value="Checkout" />
+                </form>
+                <ToastContainer />
+            </div>
         </div>
     );
 };
