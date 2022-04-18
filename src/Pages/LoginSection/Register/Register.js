0276c0c644d2caeb74675318b6e3d0a7019dc987
Tasmia-Ishika <ishika.tasmia3787@gmail.com>
2022-04-18 11:46:15 +0600

    register added

---
+++ src/Pages/LoginSection/Register/Register.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import './Register.css';
+import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import Loading from '../../Shared/Loading/Loading';
+
+const Register = () => {
+    const [agree, setAgree] = useState(false);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    const [updateProfile, updating, errorUpdate] = useUpdateProfile(auth);
+
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    if(loading || updating){
+        return <Loading></Loading>
+    }
+
+if(user){
+    console.log('user', user);
+}
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+      
+
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+        console.log('Profile Updated')
+        
+        navigate('/home');
+    }
+
+    return (
+        <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }} className='text-danger mt-4'>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input className='mt-3   ' type="text" name="name" id="" placeholder='Your Name' />
+
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+
+                <input type="password" name="password" id="" placeholder='Password' required />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Wild Earth Terms and Conditions</label>
+                <input
+                    disabled={!agree}
+                    className='w-50 mx-auto btn btn-danger mt-2'
+                    type="submit"
+                    value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+            <SocialLogin></SocialLogin>
+        </div>
+    );
+};
+
+export default Register;
